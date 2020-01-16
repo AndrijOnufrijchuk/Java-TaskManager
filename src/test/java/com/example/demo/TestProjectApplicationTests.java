fc21543ff5d842238b82aa005a15098fd8d3afb5
Happy User Mine <furaw2@gmail.com>
2020-01-16 18:31:48 +0200

    fix bugs + resolve html view bug

--- src/test/java/com/example/demo/TestProjectApplicationTests.java
+++ src/test/java/com/example/demo/TestProjectApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Base64;
 
 @SpringBootTest
 class TestProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testConfigureGlobal() throws Exception {
+        String auth = "dXNlcjpwYXNzd29yZA==";
+        System.out.println(new String(Base64.getDecoder().decode(auth)));
 
+    }
 }
