ca668f09510f95706d30858c92aec9ae765c9a27
Happy User Mine <furaw2@gmail.com>
2020-01-17 18:40:55 +0200

    Fixed mongodb bug and change security

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,27 +1,31 @@
 package com.example.demo;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/readme.txt", "/css/*").permitAll()
-                .anyRequest().authenticated()
+        http.authorizeRequests()
+                //"banned" urls
+                .antMatchers("/","/1").authenticated()
+                //"permitted" urls
+                .anyRequest().permitAll()
                 .and()
                 .formLogin().permitAll()
                 .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
+                .logout().permitAll();
+
 
     }
 
 
 }
+
